8e52d8c43a55ac24c8e5a0174c40b9c9728c33cb
Norapat Buppodom <new17353@gmail.com>
2021-02-19 22:01:51 +0700

    fix: remove unused function

--- internal/database/helper.go
+++ internal/database/helper.go
@@ -1,26 +1,12 @@
 package database
 
 import (
-	"regexp"
 	"strings"
 
 	"google.golang.org/protobuf/types/known/wrapperspb"
 	facility "onepass.app/facility/hts/facility"
 	model "onepass.app/facility/internal/model"
 )
-
-var camelRegex = regexp.MustCompile("[A-Z]?[a-z0-9]+")
-
-func camelToSnakeCase(str string) string {
-	matches := camelRegex.FindAllString(str, -1)
-	lowers := make([]string, len(matches))
-
-	for i, match := range matches {
-		lowers[i] = strings.ToLower(match)
-	}
-
-	return strings.Join(lowers, "_")
-}
 
 func convertFacilityModelToProto(data *model.Facility) *facility.Facility {
 	var operatingHours, description *wrapperspb.StringValue
